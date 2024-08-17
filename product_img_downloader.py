cff1293928cb0a528f0ef5661e631fd6e65bb518
Asib Hossen <dev.asib@proton.me>
2024-08-17 18:26:30 +0600

    Add multiple image downloading by reading from the CSV file

--- product_img_downloader.py
+++ product_img_downloader.py
@@ -1,20 +1,28 @@
+import pandas as pd
 from utils import *
+import concurrent.futures
 from seleniumbase import BaseCase
 BaseCase.main(__name__, __file__)
 
 
 class ScrapeAmazon(BaseCase):
     def test_scrape(self):
-        filename = r'links.txt'
-        with open(filename, 'r') as f:
-            links = f.readlines()
+        filename = r'src/python_book_links.csv'
+
+        # Read the CSV file and get links from the 'link' column
+        links_df = pd.read_csv(filename)
+        links = links_df['link'].tolist()
 
         for link in links:
             self.open(link)
             self.wait_for_element("h1")
             soup = self.get_beautiful_soup()
+
+            # Extract the product title
             title = soup.find(id="title")
             title = title.get_text().strip()
+
+            # Extract the product image link
             product_img = soup.find(id="landingImage")
             if product_img:
                 product_img_url = product_img.get("src")
@@ -22,9 +30,9 @@ class ScrapeAmazon(BaseCase):
                 base_url = product_img_url.split('._')[0]  # Split on '._' to remove the transformation part
                 final_url = base_url + ".jpg"  # Reconstruct the URL without the transformation part
 
+                # Saving the image to a certain directory
                 path = "images"
                 mkdir(path)
                 save_img(path, title, final_url)
             else:
                 print("Product image not found.")
-
