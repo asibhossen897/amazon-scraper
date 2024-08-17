82dfee6f9d218afb07d4e5e91d0d3337525d7691
Asib Hossen <dev.asib@proton.me>
2024-08-17 11:54:15 +0600

    Finish basic image downloading script

--- downloader.py
+++ downloader.py
@@ -1,19 +1,43 @@
+import os
+
 import requests
-from seleniumbase import Driver
-driver = Driver()
+from seleniumbase import BaseCase
+BaseCase.main(__name__, __file__)
 
 
-def scrape():
-    filename = r'links.txt'
-    with open(filename, 'r') as f:
-        links = f.readlines()
-
-    for link in links:
-        driver.open(link)
-        driver.wait_for_element("main h1")
-        soup = driver.get_beautiful_soup()
-        title = soup.select("h1")
-        print(title)
+def save_img(path, title, link):
+    with open(f"{path}/{title}.jpg", 'wb') as f:
+        f.write(requests.get(link).content)
+        print("Image saved successfully.")
 
 
-scrape()
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+class ScrapeAmazon(BaseCase):
+    def test_scrape(self):
+        filename = r'links.txt'
+        with open(filename, 'r') as f:
+            links = f.readlines()
+
+        for link in links:
+            self.open(link)
+            self.wait_for_element("h1")
+            soup = self.get_beautiful_soup()
+            title = soup.find(id="title")
+            title = title.get_text().strip()
+            product_img = soup.find(id="landingImage")
+            if product_img:
+                product_img_url = product_img.get("src")
+                # Split the URL at the last dot before the file extension
+                base_url = product_img_url.split('._')[0]  # Split on '._' to remove the transformation part
+                final_url = base_url + ".jpg"  # Reconstruct the URL without the transformation part
+
+                path = "images"
+                mkdir(path)
+                save_img(path, title, final_url)
+            else:
+                print("Product image not found.")
+
